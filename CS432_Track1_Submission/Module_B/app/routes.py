c3569af1bbc1aa6abc592d59921e4a37e39a1046
nachiketpatil0105 <nachiket1729gmail.com>
2026-03-16 01:46:01 +0530

    Updated

--- CS432_Track1_Submission/Module_B/app/routes.py
+++ CS432_Track1_Submission/Module_B/app/routes.py
@@ -1,14 +1,27 @@
 from app import app
-from flask import jsonify 
+from flask import jsonify, render_template
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def root():
     return jsonify({"message": "Welcome to test APIs"})
 
-@app.route('/login', methods = ['Post'])
+@app.route('/login', methods=['POST'])
 def login():
-    pass
+    return jsonify({
+        "message": "Login successful",
+        "session_token": "dummy_jwt_token_for_now"
+    })
 
-@app.route('/isAuth', methods = ['Get'])
+@app.route('/isAuth', methods=['GET'])
 def isAuth():
-    pass
+    return jsonify({
+        "message": "User is authenticated", 
+        "username": "test_student", 
+        "role": "Regular User", 
+        "expiry": "2026-03-15"
+    })
+
+@app.route('/portfolio', methods = ['Get'])
+def portfolio():
+    user = {'username': 'User_name'}
+    return render_template('pf.html', title = 'Portfolio', user = user)
